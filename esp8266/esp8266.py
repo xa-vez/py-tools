fada3111d04f5d8f69763d965f19a7ec6008c5f6
och <och@rpi.com>
2018-10-28 01:31:25 +0200

    [ESP8266] Function added

--- esp8266/esp8266.py
+++ esp8266/esp8266.py
@@ -1,4 +1,4 @@
- #!/usr/bin/env python
+#!/usr/bin/env python
           
 import time
 import serial
@@ -6,36 +6,36 @@ import serial
 ser = serial.Serial(
       port='/dev/ttyAMA0', baudrate = 115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)              
 
-
 class esp8266(object):
     def __init__(self):
         pass
 
-    def version(self):
-            ser.write('AT+GMR\r\n')
-            time.sleep(1)
-            x=ser.readall()
-            print x
-                
-    def scan(self, qty):
-    
-        while qty > 0 :
-            ser.write('AT+CWLAP\r\n')
-            time.sleep(5)
-            x=ser.readall()
-            print x
-            qty -= 1
-            
+    def get_response(self):
+	while 1:
+        	data=ser.readline()
+                print data
+                if (data == "ERROR\r\n" ) : break
+                if (data == "OK\r\n") : break
 
+    def version(self):
+        ser.write('AT+GMR\r\n')
+        self.get_response()
+                
+    def scan(self):
+      	ser.write('AT+CWLAP\r\n')
+	self.get_response()
+            
+    def raw(self, cmd):
+        ser.write(cmd + '\r\n')
+	self.get_response()
         
 if __name__== "__main__":
     import argparse
 
     options = argparse.ArgumentParser()
     #options.add_argument('-p', "--pdn", type=int, choices=[1,2,3], default=1, help="The PDN number")
-    #options.add_argument('-l', "--lpm", type=int, choices=[1,2,3], default=1, help="The low power mode")
-    #options.add_argument('-s', "--scan", type=str, default="", help="scan networks")
-    options.add_argument('-s', "--scan", type=int, default=1, help="scan networks -n times")
+    options.add_argument('-r', "--raw", type=str,  default="", help="raw command")
+    options.add_argument('-s', "--scan", type=int, default=0, help="scan networks -n times")
     options.add_argument('-v', "--verbose", action="store_true", help="verbose mode")
 
     args = options.parse_args()
@@ -45,8 +45,14 @@ if __name__== "__main__":
         print('verbose mode')
 
     esp = esp8266()
+    
     esp.version()
         
-    if args.scan :    
-        esp.scan(2)
+    if args.scan > 0:    
+	for x in range(0,args.scan):
+		esp.scan()
+
+    if args.raw != "" :
+    	esp.raw(cmd=args.raw)
+
 
