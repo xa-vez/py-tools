ffa5da7a4c059c9fb9e4bf61a0e5e795d7a1c171
ochavez <och@ubuntu.com>
2018-10-27 19:38:19 -0400

    [ESP8266] file format

--- esp8266/esp8266.py
+++ esp8266/esp8266.py
@@ -4,17 +4,19 @@ import time
 import serial
               
 ser = serial.Serial(
-      port='/dev/ttyAMA0', baudrate = 115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)              
+      port='/dev/ttyAMA0', baudrate=115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)              
 
 class esp8266(object):
+
     def __init__(self):
         pass
 
     def get_response(self):
+
 	while 1:
-        	data=ser.readline()
+        	data = ser.readline()
                 print data
-                if (data == "ERROR\r\n" ) : break
+                if (data == "ERROR\r\n") : break
                 if (data == "OK\r\n") : break
 
     def version(self):
@@ -23,36 +25,30 @@ class esp8266(object):
                 
     def scan(self):
       	ser.write('AT+CWLAP\r\n')
-	self.get_response()
+        self.get_response()
             
     def raw(self, cmd):
         ser.write(cmd + '\r\n')
-	self.get_response()
-        
-if __name__== "__main__":
+        self.get_response()
+
+if __name__ == "__main__":
     import argparse
 
     options = argparse.ArgumentParser()
-    #options.add_argument('-p', "--pdn", type=int, choices=[1,2,3], default=1, help="The PDN number")
-    options.add_argument('-r', "--raw", type=str,  default="", help="raw command")
+    options.add_argument('-r', "--raw", type=str, default="", help="raw command")
     options.add_argument('-s', "--scan", type=int, default=0, help="scan networks -n times")
     options.add_argument('-v', "--verbose", action="store_true", help="verbose mode")
-
     args = options.parse_args()
-        
-    if args.verbose :
-        #print( 'verbose mode is runnig ' + args.domain + ' at PDN ' + str(args.pdn))
-        print('verbose mode')
 
     esp = esp8266()
-    
     esp.version()
+    
+    if args.raw != "" :
+        esp.raw(cmd=args.raw)
         
     if args.scan > 0:    
-	for x in range(0,args.scan):
-		esp.scan()
+	    for x in range(0, args.scan):
+            esp.scan()
 
-    if args.raw != "" :
-    	esp.raw(cmd=args.raw)
 
 
